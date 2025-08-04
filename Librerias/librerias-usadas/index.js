6b6d3ee4cd8f46d4a8fceebf01c832781285f40f
HenryLudena <133303666+HenryLudena@users.noreply.github.com>
2025-08-03 19:25:41 -0500

    Create index.js

---
+++ Librerias/librerias-usadas/index.js
@@ -0,0 +1,16 @@
+//Suma
+const paqueteSuma = require('2025a-swgr1-hflc-suma');
+const respuesta = paqueteSuma.suma(2, 1);
+console.log('La respuesta es de la suma: ', respuesta);
+//División
+const paqueteDivision = require("2025a-swgr1-hflc-division");
+const respuestaDivision = paqueteDivision.division(9, 3);
+console.log("La respuesta de la división: ", respuestaDivision);
+//Multiplicación
+const paqueteMultiplicacion = require("2025a-swgr1-jdcj-multiplicacion");
+const respuestaMultiplicacion = paqueteMultiplicacion.multiplicacion(2, 5);
+console.log("La respuesta de la multiplicación: ", respuestaMultiplicacion);
+//Resta
+const paqueteResta = require("2025a-swgr1-jdcj-resta");
+const respuestaResta = paqueteResta.resta(3, 2);
+console.log("La respuesta de la resta: ", respuestaResta);
